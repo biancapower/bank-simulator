067f2e8e0a0405cc163da55b1b3663b7610bba71
Bianca Power <drbiancampower@gmail.com>
2018-05-21 15:14:13 +1000

    add getDetails and withdrawal functions

--- scripts.js
+++ scripts.js
@@ -37,7 +37,35 @@ const accounts = [];
 
 for (let i = 0; i < 10; i++) {
     // create new account
-    let newAccount = new Account(randomName(), randomInt(10000000, 99999999), randomInt(0, 1000000000000));
+    let newAccount = new Account(randomName(), randomInt(10000000, 99999999), randomInt(0, 10000));
 
     accounts.push(newAccount);
+}
+
+function getDetails() {
+    const accountNumber = document.getElementById("accNum").value;
+
+    for (let i = 0; i < accounts.length; i++) {
+        if (accounts[i].number == accountNumber) {
+            document.getElementById("details").innerHTML = `<p>Name: ${accounts[i].name}</p><p>Balance: $${accounts[i].balance}</p>`;
+            return;
+        }
+    }
+
+    document.getElementById("details").innerHTML = "Not a valid account number";    
+}
+
+function withdrawal() {
+    const accountNumber = document.getElementById("accNum").value;
+
+    for (let i = 0; i < accounts.length; i++) {
+        if (accounts[i].number == accountNumber) {
+            accounts[i].withdraw(1000);
+
+            getDetails();
+            return;
+        }
+    }
+
+    document.getElementById("details").innerHTML = "Not a valid account number";        
 }
